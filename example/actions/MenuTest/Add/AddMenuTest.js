8a11626359846bb4c4257d6ec55aa3a529923e20
Jonathan Larsen <jonathan@ninecollective.com>
2014-11-18 17:56:18 -0600

    Testing new array and multiple menu locations

--- example/actions/MenuTest/Add/AddMenuTest.js
+++ example/actions/MenuTest/Add/AddMenuTest.js
@@ -1,8 +1,12 @@
-exports.menu = {
+exports.menu = [{
     rootNavigation: 'Sales',
     subMenuName: 'Bids__and__Quotes',
     newWindow: true
-};
+}, {
+    rootNavigation: 'Test',
+    subMenuName: 'Dog',
+    newWindow: false
+}];
 exports.permission = 'AddMenuTest';
 exports.permissionMiddleware = require('../../../middleware').lookup(this.permission);
 
